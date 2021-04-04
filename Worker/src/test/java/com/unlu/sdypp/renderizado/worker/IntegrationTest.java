f7a76c5d49f9cdcf21018333a7047cf4f111bccb
Nicolás Larena <nic.lar.universidad@gmail.com>
2021-04-03 23:01:24 -0300

    RDI-23

---
+++ Worker/src/test/java/com/unlu/sdypp/renderizado/worker/IntegrationTest.java
@@ -0,0 +1,10 @@
+package com.unlu.sdypp.renderizado.worker;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegrationTest {
+}
