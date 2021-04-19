7499c4170309f626e9b01c5ae659bc89a9c39fbd
Nicolás Larena <nic.lar.universidad@gmail.com>
2021-04-19 11:16:31 -0300

    Flapdoodle added

--- FileServer/test/unit/java/com/unlu/sdypp/renderizado/fileserver/entities/datastores/FileDatastoreTest.java
+++ FileServer/test/unit/java/com/unlu/sdypp/renderizado/fileserver/entities/datastores/FileDatastoreTest.java
@@ -6,11 +6,16 @@ import com.unlu.sdypp.renderizado.fileserver.repositories.FileRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class FileDatastoreTest extends IntegrationTest {
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class FileDatastoreTest /*extends IntegrationTest*/ {
 
     @Autowired
     private FileRepository repository;
